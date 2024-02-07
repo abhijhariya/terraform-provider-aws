6740c57555d683ca91dc15e8a58c0adf2accb657
abhilash <abhilash.zariya@intellore.com>
2024-02-07 17:36:03 +0530

    chnages commited

--- examples/github/github.tf
+++ examples/github/github.tf
@@ -13,6 +13,4 @@ provider "gitlab" {
 resource "gitlab_project" "myrepo_terraform" {
   name          = "first_terraform_repo"
   description   = "repo for demo"
-  visibility    = "private"
-  auto_init     = true
 }
